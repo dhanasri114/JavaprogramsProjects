f450a97fd8bef94899fa7dbfc920251f823bac5b
dhanasrisai <dhanusreeramdas@gmail.com>
2026-07-06 19:13:13 +0530

    "email add"

--- src/main/java/org/java/programs/Construct.java
+++ src/main/java/org/java/programs/Construct.java
@@ -5,21 +5,23 @@ public class Construct
 	int id;
 	String name;
 	int age;
+	String email;
 	
-	Construct(int id,String name, int age)
+	Construct(int id,String name, int age,String email)
 	{
 		this.id=id;
 		this.name=name;
 		this.age=age;
+		this.email=email;
 	}
 	
 	void student()
 	{
-		System.out.println("my name :"+name+"\n"+"my age was:"+age+"\n");
+		System.out.println("my name :"+name+"\n"+"my age was:"+age+"\n"+"my email is :"+email);
 	}
 	
 	public static void main(String[] args) 
 	{
-		new Construct(1,"dhanu",22).student();
+		new Construct(1,"dhanu",22,"dhaniu@gmail.com").student();
 	}
 }
