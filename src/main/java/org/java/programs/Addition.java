d89bb5ba6c7c3c4c6ac6a130b350093ed6f9abd2
dhanasrisai <dhanusreeramdas@gmail.com>
2026-07-06 15:03:42 +0530

    "addition program"

---
+++ src/main/java/org/java/programs/Addition.java
@@ -0,0 +1,28 @@
+package org.java.programs;
+
+
+public class Addition 
+{
+	
+	int num1;
+	int num2;
+	
+	int result;
+	
+	Addition(int num1,int num2)
+	{
+		this.num1=num1;
+		this.num2=num2;
+	}
+	
+	
+	int add()
+	{
+		return num1+num2;
+	}
+	
+    public static void main( String[] args )
+    {
+        Addition a=new Addition(20, 30);
+    }
+}
